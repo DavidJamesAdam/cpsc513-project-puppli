7b5718d1d9953ad562416923c4f5bdf6d4a94f06
David Adam <david.adam1@ucalgary.ca>
2025-12-06 16:30:16 -0700

    refactor: updated routers folder

--- backend/main.py
+++ backend/main.py
@@ -6,33 +6,33 @@ from pydantic import BaseModel
 
 from firebase_service import db
 
-from handlers.auth.login import router as auth_login_router
-from handlers.auth.logout import router as auth_logout_router
+from routers.auth.login import router as auth_login_router
+from routers.auth.logout import router as auth_logout_router
 
-from handlers.posts.createPost import router as create_post_router
-from handlers.posts.getPosts import router as get_posts_router
-from handlers.posts.getUserPosts import router as get_user_posts_router
-from handlers.posts.addComment import router as add_comment_router
-from handlers.posts.awardMedals import router as award_medals_router
-from handlers.posts.postVote import router as post_vote_router
-from handlers.posts.postFavourite import router as post_favourite_router
-from handlers.posts.rankGlobal import router as rank_global_router
-from handlers.posts.rankProvince import router as rank_province_router
-from handlers.posts.rankCity import router as rank_city_router
+from routers.posts.createPost import router as create_post_router
+from routers.posts.getPosts import router as get_posts_router
+from routers.posts.getUserPosts import router as get_user_posts_router
+from routers.posts.addComment import router as add_comment_router
+from routers.posts.awardMedals import router as award_medals_router
+from routers.posts.postVote import router as post_vote_router
+from routers.posts.postFavourite import router as post_favourite_router
+from routers.posts.rankGlobal import router as rank_global_router
+from routers.posts.rankProvince import router as rank_province_router
+from routers.posts.rankCity import router as rank_city_router
 
-from handlers.users.getUser import router as get_user_router
-from handlers.users.updateUser import router as update_user_router
-from handlers.users.postUser import router as post_user_router
-from handlers.users.deleteUser import router as delete_user_router
+from routers.users.getUser import router as get_user_router
+from routers.users.updateUser import router as update_user_router
+from routers.users.postUser import router as post_user_router
+from routers.users.deleteUser import router as delete_user_router
 
-from handlers.pets.getPets import router as get_pets_router
-from handlers.pets.updatePet import router as update_pet_router
-from handlers.pets.createSubprofile import router as create_subprofile_router
-from handlers.pets.getPetByID import router as get_pet_by_id_router
-from handlers.pets.deleteSubprofile import router as delete_subprofile_router
+from routers.pets.getPets import router as get_pets_router
+from routers.pets.updatePet import router as update_pet_router
+from routers.pets.createSubprofile import router as create_subprofile_router
+from routers.pets.getPetByID import router as get_pet_by_id_router
+from routers.pets.deleteSubprofile import router as delete_subprofile_router
 
-from handlers.auth.updateEmail import router as update_email_router
-from handlers.auth.updatePassword import router as update_password_router
+from routers.auth.updateEmail import router as update_email_router
+from routers.auth.updatePassword import router as update_password_router
 
 from utils.authCheck import router as auth_check_router
 
