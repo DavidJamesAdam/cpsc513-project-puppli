3df2fa73ddaab556176b1a5c007f369b86ff3e65
David Adam <david.adam1@ucalgary.ca>
2026-06-10 16:34:06 -0600

    Added more comprehensive logging structure

--- backend/main.py
+++ backend/main.py
@@ -1,69 +1,74 @@
 import logging
+import sys
+import os
+import contextvars
+import uuid
+from starlette.middleware.base import BaseHTTPMiddleware
+from logging.handlers import RotatingFileHandler
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from api import api_router
 from limiter import limiter
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi.errors import RateLimitExceeded
+from pythonjsonlogger import jsonlogger
 
-# # TODO: Right now, logs are written to myapp.log, but I would love to have the logs exported to an
-# # external service to be read (Cloudwatch, Grafana, etc)
-# # Handlers are a way to do this
-logging.basicConfig(filename='myapp.log',
-                    # stream=sys.stdout,
-                    level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S' # Custom date format
-                    )
+# TODO: Right now, logs are written to myapp.log, but I would love to have the logs exported to an
+# external service to be read (Cloudwatch, Grafana, etc)
+# Handlers are a way to do this
+# logging.basicConfig(
+#                     # stream=sys.stdout,
+#                     level=logging.INFO,
+#                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#                     datefmt='%Y-%m-%d %H:%M:%S', # Custom date format,
+#                     handlers=[RotatingFileHandler("../myapp.log", maxBytes=10*1024*1024, backupCount=5)]
+#                     )
 
-# REQUEST_ID_CTX = contextvars.ContextVar("request_id", default=None)
+REQUEST_ID_CTX = contextvars.ContextVar("request_id", default=None)
 
-# class RequestIdFilter(logging.Filter):
-#     def filter(self, record):
-#         record.request_id = REQUEST_ID_CTX.get()
-#         return True
+class RequestIdFilter(logging.Filter):
+    def filter(self, record):
+        record.request_id = REQUEST_ID_CTX.get()
+        return True
 
-# LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
 
-# def configure_logging():
-#     root = logging.getLogger()
-#     if root.handlers:
-#         return
-#     root.setLevel(LOG_LEVEL)
+def configure_logging():
+    root = logging.getLogger()
+    if root.handlers:
+        return
+    root.setLevel(LOG_LEVEL)
 
-#     fmt = '%(asctime)s %(name)s %(levelname)s %(message)s %(request_id)s'
-#     json_formatter = jsonlogger.JsonFormatter(fmt)
+    format = '%(asctime)s %(name)s %(levelname)s %(message)s %(request_id)s'
+    json_formatter = jsonlogger.JsonFormatter(format)
 
-#     sh = logging.StreamHandler(sys.stdout)
-#     sh.setLevel(LOG_LEVEL)
-#     sh.setFormatter(json_formatter)
+    streamHandler = logging.StreamHandler(sys.stdout)
+    streamHandler.setLevel(LOG_LEVEL)
+    streamHandler.setFormatter(json_formatter)
 
-#     fh = RotatingFileHandler("myapp.log", maxBytes=10*1024*1024, backupCount=5)
-#     fh.setLevel(LOG_LEVEL)
-#     fh.setFormatter(json_formatter)
+    fileHandler = RotatingFileHandler("../myapp.log", maxBytes=10*1024*1024, backupCount=5)
+    fileHandler.setLevel(LOG_LEVEL)
+    fileHandler.setFormatter(json_formatter)
 
-#     rfid = RequestIdFilter()
-#     sh.addFilter(rfid)
-#     fh.addFilter(rfid)
+    requestIdFilter = RequestIdFilter()
+    streamHandler.addFilter(requestIdFilter)
+    fileHandler.addFilter(requestIdFilter)
 
-#     root.addHandler(sh)
-#     root.addHandler(fh)
+    root.addHandler(streamHandler)
+    root.addHandler(fileHandler)
 
-# # call at startup
-# configure_logging()
+# call at startup
+configure_logging()
 
-# # FastAPI middleware to set request id per request
-# from starlette.middleware.base import BaseHTTPMiddleware
-
-# class RequestIDMiddleware(BaseHTTPMiddleware):
-#     async def dispatch(self, request: Request, call_next):
-#         rid = request.headers.get("X-Request-ID") or str(uuid.uuid4())
-#         REQUEST_ID_CTX.set(rid)
-#         response = await call_next(request)
-#         response.headers["X-Request-ID"] = rid
-#         return response
+class RequestIDMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        rid = request.headers.get("X-Request-ID") or str(uuid.uuid4())
+        REQUEST_ID_CTX.set(rid)
+        response = await call_next(request)
+        response.headers["X-Request-ID"] = rid
+        return response
 
 
 @asynccontextmanager
